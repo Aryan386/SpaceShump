d48dc41e2558c1d5d9243f20b74e532ac8e21db1
Sandeep Upadhyaya <sandeep.upadhyaya85@gmail.com>
2018-06-02 18:24:53 +0530

    New project space shump test

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform Ptarget;
+    public float smoothSpeed = 0.5f;
+    public Vector3 offset;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 desiredPosition = Ptarget.position + offset;
+       Vector3 smoothPosition = Vector3.Slerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        // Camera.main.
+        transform.position = smoothPosition;
+        //transform.position = desiredPosition;
+		
+	}
+}
---
+++ Assets/PilotPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PilotPlayer : MonoBehaviour {
+
+    public float Pspeed = 900f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //camera follow up
+
+       // Vector3 moveCamTo = transform.position - transform.forward * 20f + Vector3.up * 5f * Time.deltaTime;
+        //Camera.main.transform.position = moveCamTo;
+        //Camera.main.transform.LookAt(transform.position + transform.position*30);
+
+        transform.position += transform.forward * Time.deltaTime * Pspeed;
+        Pspeed -= transform.forward.y * 50f * Time.deltaTime;
+
+        if (Pspeed < 35f)
+        {
+            Pspeed = 35f;
+        }
+
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+		
+	}
+}
